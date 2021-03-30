fd48bcf540ee2ecdd8abb45d061853ce23c469df
JohnnyB0Y <ag860050872@163.com>
2021-03-30 20:04:34 +0800

    LFU实现

--- concepts/cache/cache.js
+++ concepts/cache/cache.js
@@ -4,7 +4,7 @@
 //  Created by JohnnyB0Y on 2021/03/30.
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
-// LRU --------------------------------------------------
+// LRU: Least Recently used
 
 class LRUNode {
   constructor(key, data) {
@@ -113,7 +113,7 @@ class LRUCache {
   _printNodes() {
 
     let node = this.head.next
-    console.log('count:', this.count)
+    console.log('count:', this.count, this.data.size)
     while (node) {
       console.log(`${node.key}: ${node.data}`)
       node = node.next
@@ -148,3 +148,215 @@ lruCache.addNode(16, 'Node - 16')
 
 // 15, 16, 3, 0 -- 9 ~ 4
 lruCache._printNodes()
+
+
+// LFU: Least Frequency Used
+
+class LFUNode {
+  constructor(key, data) {
+    this.left = null
+    this.right = null
+    this.prev = null
+
+    this.key = key
+    this.data = data
+    this.frequency = 1
+  }
+}
+
+class LFUCache {
+  constructor(capacity) {
+    this.capacity = capacity
+    this.count = 0
+    this.head = new LFUNode()
+    this.data = new Map()
+  }
+
+  getNode(key) {
+    // 从哈希表中取
+    const node = this.data.get(key)
+    if (node) {
+      node.frequency += 1
+      this._shift(node)
+      return node.data
+    }
+    return null
+  }
+
+  addNode(key, data) {
+    let node = this.data.get(key)
+    if ( node ) {
+      node.data = data
+      return
+    }
+
+    node = new LFUNode(key, data)
+    if (!this.head.prev) {
+      // 第一个
+      this.head.left = node
+      this.head.prev = node
+      node.prev = this.head
+    }
+    else {
+      // 淘汰？
+      let last = this.head.prev
+      if (this.count >= this.capacity) {
+        if (last && last.key) {
+          this.data.delete(last.key)
+        }
+        last = last.prev
+        this.count--
+      }
+      last.left = node
+      node.prev = last
+      this.head.prev = node
+    }
+
+    this.data.set(key, node)
+    this.count++
+  }
+
+  _shift(node) {
+    if (node.prev.left === node) {
+      this._upshift(node)
+    }
+    else {
+      this._downshift(node)
+    }
+  }
+
+  // 左up
+  _upshift(node) {
+
+    const prev = node.prev
+    if (prev === this.head) {
+      return
+    }
+
+    // 大于，就转换位置
+    if (node.frequency > prev.frequency) {
+      const prevRight = prev.right
+      const curLeft = node.left
+      const curRight = node.right
+      const superPrev = prev.prev
+      
+      node.prev = superPrev
+      if (superPrev.left === prev) {
+        superPrev.left = node
+      }
+      else {
+        superPrev.right = node
+      }
+      
+      // 先交接自己孩子
+      prev.left = curLeft
+      prev.right = curRight
+      if (curLeft) {
+        curLeft.prev = prev
+      }
+      if (curRight) {
+        curRight.prev = prev
+      }
+      
+      // 再交接上级的孩子
+      node.left = prev
+      node.right = prevRight
+      prev.prev = node
+      if (prevRight) {
+        prevRight.prev = node
+      }
+      
+      if (node === this.head.prev) {
+        this.head.prev = prev
+      }
+      
+      // 继续
+      this._shift(node)
+    }
+  }
+
+  // 右 down
+  _downshift(node) {
+    console.log(node)
+    const child = node.right
+    if (child && node.frequency > child.frequency) {
+      const childLeft = child.left
+      const childRight = child.right
+      const curLeft = node.left
+      const superPrev = node.prev
+
+      child.prev = superPrev
+      if (superPrev.left === node) {
+        superPrev.left = child
+      }
+      else {
+        superPrev.right = child
+      }
+
+      // 先交接孩子的孩子
+      node.left = childLeft
+      node .right = childRight
+      if (childLeft) {
+        childLeft.prev = node
+      }
+      if (childRight) {
+        childRight.prev = node
+      }
+
+      // 在交接自己的孩子
+      child.left  = curLeft
+      child.right = node
+      if (curLeft) {
+        curLeft.prev = child
+      }
+      node.prev = child
+
+      // 继续和右孩子比大小
+      this._downshift(node)
+    }
+  }
+
+  _printNodes() {
+
+    let node = this.head.left
+    console.log('count:', this.count, this.data.size)
+    this._printNodeChild(node)
+    console.log('-------------------------')
+  }
+
+  _printNodeChild(node) {
+
+    if (!node) return;
+    const left = node.left
+    const right = node.right
+
+    this._printNodeChild(right)
+    console.log(`${node.key}: ${node.data} - ${node.frequency} 次`)
+    this._printNodeChild(left)
+  }
+
+}
+
+const lfuCache = new LFUCache(10)
+for (let i = 0; i < 10; i++) {
+  lfuCache.addNode(i, `Node`)
+}
+
+lfuCache._printNodes()
+
+lfuCache.getNode(0)
+
+lfuCache._printNodes()
+
+lfuCache.addNode(15, 'Node')
+
+lfuCache.getNode(0)
+lfuCache.getNode(0)
+lfuCache.getNode(0)
+lfuCache.getNode(1)
+lfuCache.getNode(15)
+lfuCache.getNode(15)
+lfuCache.getNode(15)
+lfuCache.getNode(5)
+
+lfuCache._printNodes()
