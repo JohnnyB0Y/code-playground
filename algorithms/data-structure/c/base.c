d61b25aa90cccdac4c45c5f39fddf37a92119c8d
JohnnyB0Y <ag860050872@163.com>
2021-05-09 23:57:33 +0800

    heap

--- algorithms/data-structure/c/base.c
+++ algorithms/data-structure/c/base.c
@@ -4,6 +4,10 @@
 //  Created by JohnnyB0Y on 2021/05/06.
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
+#include <stdbool.h>
+
 typedef void * id;
 
 typedef void (*iteration_func)(id item, int idx);
+
+typedef bool (*compare_func)(id val1, id val2);
--- algorithms/data-structure/c/heap.c
+++ algorithms/data-structure/c/heap.c
@@ -4,8 +4,36 @@
 //  Created by JohnnyB0Y on 2021/05/09.
 //  Copyright © 2021 JohnnyB0Y. All rights reserved.
 
-#include "base.c"
+#include "array.c"
 
-struct heap {
-  id 
-};
+void heap_down_adjust(array_t arr, int parent_index, compare_func func);
+
+void heap_init(array_t arr, compare_func func) {
+  // 从最后一个非叶子节点开始，依次下沉调整
+  for (int i = arr->count / 2; i >= 0; i--) {
+    heap_down_adjust(arr, i, func);
+  }
+}
+
+void heap_up_adjust(array_t arr, int parent_index, int count) {
+  
+}
+
+void heap_down_adjust(array_t arr, int parent_index, compare_func func) {
+  id parent = array_get_item(arr, parent_index);
+  int child_index = 2 * parent_index + 1;
+  while (child_index < arr->count) {
+    if (child_index + 1 < arr->count) {
+      // 有右孩子，比较左右孩子
+      if (func(array_get_item(arr, child_index), array_get_item(arr, child_index + 1))) {
+        child_index++;
+      }
+
+      if (func(parent, array_get_item(arr, child_index))) {
+        break;
+      }
+
+      // ... 
+    }
+  }
+}
